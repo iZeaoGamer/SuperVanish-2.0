c47f538d75d33e6a94d668a0405e9628ff70e1c6
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-03 17:05:00 +0100

    Bug fixes.

--- src/Vanish/Main.php
+++ src/Vanish/Main.php
@@ -25,17 +25,22 @@ if($sender->hasPermission("supervanish.spectate")){
     if($args[0] == "on"){
  $sender->setNameTagVisible(false);
  $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), (99999999*20), (1), (false)));
- $sender->getPlayer()->addTitle("§l§7Vanish has been", "§l§7Turned - §aON", 40, 100, 40);
+ $sender->getPlayer()->addActionBarMessage("§dSupervanish has been turned on. \n§cNo one can see you.");
+ $sender->sendMessage("§7[§aSuper§6Vanish§7] §dVanish mode has been enabled. §5No one can see you. :)");
+ $sender->getPlayer()->addTitle("§l§7Vanish has been", "§l§7Turned - §aON", 1000, 100000, 10000);
  return true;
         }
    if($args[0] == "off"){
  $sender->setNameTagVisible(true);
  $sender->removeEffect(Effect::INVISIBILITY);
  $sender->getPlayer()->addTitle("§l§7Vanish has been", "§l§7Turned - §aOFF", 40, 100, 40);
+ $sender->getPlayer()->removeTitles(true);
+ $sender->getPlayer()->addActionBarMessage("§dSupervanish has been turned off. \n§bEveryone can see you now.");
+ $sender->sendMessage("§dSuperVanish has been turned off. §bEveryone can see you.");
  return true;
      }
    }else{
-    $sender->sendMessage("§aPlease use: §b/vanish <on|off> §6to turn on / off vanish.");
+    $sender->sendMessage("§aPlease use: §b/$cmd <on|off> §6to turn on / off vanish.\n§aYou can also use: §b/$cmd about §6to see information about this plugin.");
     return true;
            }
     if($args[0] == "about"){
@@ -46,7 +51,7 @@ if($sender->hasPermission("supervanish.spectate")){
   return true;
     }
   }else{
-    $sender->sendMessage("Please use this command in game");
+   $sender->sendMessage("Please use this command in game");
    return true;
    }
   }
